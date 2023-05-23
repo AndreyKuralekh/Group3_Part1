a825ed7975147902ada1c4ba33b5a6b04a1fd255
Andrii.Kuralekh <Andrii.Kuralekh@AKURALEKH-N>
2023-05-23 19:25:34 +0300

    Add project files.

---
+++ Group3_Part1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Group3_Part1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value:");
+            string? test = Console.ReadLine();
+            double testDouble;
+
+            if (double.TryParse(test, out testDouble))
+            {
+                Console.WriteLine("The value is: {0}", testDouble);
+            }
+            else 
+            {
+                Console.WriteLine("Couldn`t convert");
+            }
+        }
+    }
+}
