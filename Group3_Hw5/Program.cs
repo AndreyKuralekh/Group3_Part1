c4802aa66fadb81c064e97837966d63d5f1ea89a
Andrii.Kuralekh <Andrii.Kuralekh@AKURALEKH-N>
2023-06-05 22:24:34 +0300

    HW 5 has been complited

---
+++ Group3_Hw5/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Group3_Hw5
+{
+    /*
+        Create a student class and describe its main characteristics: 
+            - First name, 
+            - last name, 
+            - age, 
+            - city, 
+            - courses attended (an array of courses).
+        
+        Describe the following actions: Print (output basic information).
+        
+        Add one course to the student (add a new course to the course array).
+        Delete a course from a student.
+       
+        Create a course class and describe its main characteristics: 
+            - course name, 
+            - teacher name, 
+            - course duration, 
+            - number of students.
+        
+        Describe the following actions: Print (output basic information).
+        
+        Add a student to the course group (add one student to the number of students).
+
+    for each of the classes create a chain of calling constructors.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Courses[] courses = new Courses[10];
+            courses[0] = new Courses("History", "Ivanupollo", 4, 0);
+            courses[1] = new Courses("Match", "Ivanoff", 12, 0);
+            courses[2] = new Courses("Philosofy", "Petroff", 8, 0);
+            courses[3] = new Courses("Architecture", "Sidoroff", 6, 0);
+            courses[4] = new Courses("Dentistry", "Petrenko", 22, 0);
+            courses[5] = new Courses("Economics", "Ivanoff", 3, 0);
+            courses[6] = new Courses("Law", "Putinkoff", 15, 0);
+            
+            //------------------------------------------------------------------------
+            var student1 = new Student("Taras","Shevchenko",22, "Kiev");
+            student1.AddNewCourse(courses[0]);
+            student1.AddNewCourse(courses[1]);
+            student1.AddNewCourse(courses[2]);
+            student1.PrintInfo();
+            //------------------------------------------------------------------------
+            var student2 = new Student("Petro", "Petroff", 26, "Lviv");
+            student2.AddNewCourse(courses[1]);
+            student2.AddNewCourse(courses[6]);
+            student2.PrintInfo();
+            //------------------------------------------------------------------------
+
+
+            student1.DeleteCourse("Match");
+            student1.PrintInfo();
+            student2.PrintInfo();
+            //
+
+            Console.WriteLine($"finish");
+
+        }
+    }
+}
---
+++ Group3_Hw5/Student.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group3_Hw5
+{
+    internal class Student
+    {
+        private string FirstName { get; set; }
+        private string LastName { get; set; } 
+        private int Age { get; set; } 
+        private string City { get; set; } 
+
+        private Courses[] CoursesAttended = new Courses[10];
+
+        public Student(): this("NoBody") 
+        {
+            Console.WriteLine("Contructor reload 1");
+        }
+        public Student(string firstName) :this(firstName, "NoBody")
+        {
+            this.FirstName = firstName;
+            //Console.WriteLine("Contructor reload 2");
+        }
+        public Student(string firstName, string lastName) : this(firstName, lastName, 0)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            //Console.WriteLine("Contructor reload 3");
+        }
+        public Student(string firstName, string lastName, int age) : this(firstName, lastName, age, "Nowhere")
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            //Console.WriteLine("Contructor reload 4");
+        }
+        public Student(string firstName, string lastName, int age, string city) 
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.City = city;
+            //Console.WriteLine("Contructor reload 5");
+        }
+        public Student(string firstName, string lastName, int age, string city, Courses[] coursesAttended)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.City = city;
+            this.CoursesAttended = coursesAttended;
+            //Console.WriteLine("Contructor reload 6");
+        }
+
+        //output basic information
+        public void PrintInfo () 
+        {
+            Console.WriteLine("============================================");
+
+            Console.WriteLine("============================================");
+            Console.WriteLine("Basic student`s info:");
+            Console.WriteLine("============================================");
+            Console.WriteLine($"FirstName: {this.FirstName}");
+            Console.WriteLine($"LastName: {this.LastName}");
+            Console.WriteLine($"Age: {this.Age}");
+            Console.WriteLine($"City: {this.City}");
+
+            Console.WriteLine("============================================");
+            Console.WriteLine("Courses list:");
+            Console.WriteLine("============================================");
+            int i = 1;
+            foreach (var cousrce in CoursesAttended)
+            {
+                if (!(cousrce == null))
+                {
+                    Console.Write($"{i++}.");
+                    cousrce.PrintCourceInfo();
+                }
+            }
+
+        }
+        //Add one course to the student(add a new course to the course array).
+        public void AddNewCourse(Courses newCource ) 
+        {
+            int i = 0;
+            foreach (var cource in this.CoursesAttended)
+            {
+                if (cource == null)
+                {
+                    this.CoursesAttended[i] = newCource; 
+                    this.CoursesAttended[i].AddOneStudent();
+                    break;
+                }   
+                i++;
+            }
+        }
+        //Delete a course from a student.
+        public void DeleteCourse(string courceName)
+        {
+            int i = 0;
+            int findOf = 0;
+            foreach (var cource in this.CoursesAttended)
+            {
+                if (cource.CourseName == courceName)
+                {
+                    findOf = i;
+                    break;
+                }
+                i++;
+            }
+            this.CoursesAttended = this.CoursesAttended.Where((val, idx) => idx != findOf).ToArray();
+        }
+    }
+}
